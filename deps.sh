a64b61f26955ae577192ca3e1c0402d86f1660ff
florensh <florensh@gmx.de>
2016-03-19 22:03:57 +0100

    test

--- deps.sh
+++ deps.sh
@@ -3,6 +3,6 @@
 set -o errexit
 set -o pipefail
 
-sudo apt-get update
-sudo apt-get install -y libssl-dev tshark aircrack-ng iw
-sudo airmon-ng start wlan0
+# sudo apt-get update
+# sudo apt-get install -y libssl-dev tshark aircrack-ng iw
+# sudo airmon-ng start wlan0
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-var spawn = require('child_process').spawn,
-  ts = spawn('tshark', ['-i', 'mon0', '-I', '-f', 'broadcast', '-R', 'wlan.fc.type == 0 && wlan.fc.subtype == 4', '-T', 'fields', '-e', 'frame.time_epoch', '-e', 'wlan.sa', '-e', 'radiotap.dbm_antsignal']);
+// var spawn = require('child_process').spawn,
+//   ts = spawn('tshark', ['-i', 'mon0', '-I', '-f', 'broadcast', '-R', 'wlan.fc.type == 0 && wlan.fc.subtype == 4', '-T', 'fields', '-e', 'frame.time_epoch', '-e', 'wlan.sa', '-e', 'radiotap.dbm_antsignal']);
+//
+//
+//
+// ts.stdout.on('data', function(data) {
+//   console.log('stdout: ' + data);
+// });
+//
+// ts.stderr.on('data', function(data) {
+//   console.log('stderr: ' + data);
+// });
+//
+// ts.on('exit', function(code) {
+//   console.log('child process exited with code ' + code);
+// });
 
 
-
-ts.stdout.on('data', function(data) {
-  console.log('stdout: ' + data);
-});
-
-ts.stderr.on('data', function(data) {
-  console.log('stderr: ' + data);
-});
-
-ts.on('exit', function(code) {
-  console.log('child process exited with code ' + code);
-});
+console.log('los gehts!');
