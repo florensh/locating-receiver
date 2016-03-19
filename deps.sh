41aba002f9cd8ee749e0083e2bcc7bb13689ddf0
florensh <florensh@gmx.de>
2016-03-19 22:34:47 +0100

    test

--- deps.sh
+++ deps.sh
@@ -4,5 +4,11 @@ set -o errexit
 set -o pipefail
 
 sudo apt-get update
-sudo apt-get install -y libssl-dev tshark aircrack-ng iw
+sudo apt-get install -y libssl-dev tshark
+wget http://download.aircrack-ng.org/aircrack-ng-1.2-beta1.tar.gz
+tar -zxvf aircrack-ng-1.2-beta1.tar.gz
+cd aircrack-ng-1.2-beta1
+make
+make install
+apt-get -y install iw
 sudo airmon-ng start wlan0
