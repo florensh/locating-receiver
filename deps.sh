cd3029bf588c4f3b51b9c49d75856f3fee246926
florensh <florensh@gmx.de>
2016-03-19 21:45:02 +0100

    deps script fixed

--- deps.sh
+++ deps.sh
@@ -4,6 +4,6 @@ set -o errexit
 set -o pipefail
 
 sudo apt-get update
-sudo apt-get install -y install libssl-dev tshark aircrack-ng iw
+sudo apt-get install -y libssl-dev tshark aircrack-ng iw
 sudo airodump-ng-oui-update
 sudo airmon-ng start wlan0
