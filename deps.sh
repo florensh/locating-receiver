d488c58416c1f85060e8fe051059a393e779ec9c
florensh <florensh@gmx.de>
2016-03-19 23:10:35 +0100

    test

--- deps.sh
+++ deps.sh
@@ -4,7 +4,6 @@ set -o errexit
 set -o pipefail
 
 sudo apt-get update
-echo "yes" | sudo debconf-set-selections
 sudo apt-get install -y libssl-dev aircrack-ng iw
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q install tshark
 sudo airmon-ng start wlan0
