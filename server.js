4ff797ce4ba9d945d3e9ec648c498097df724b40
florensh <florensh@gmx.de>
2016-03-20 13:00:31 +0100

    test

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ console.log('starting tshark');
 var sys = require('sys'),
   exec = require('child_process').exec;
 exec('sudo airmon-ng start wlan1', function(error, stdout, stderr) {
+
   if (!error) {
     // print the output
     sys.puts(stdout);
@@ -9,7 +10,7 @@ exec('sudo airmon-ng start wlan1', function(error, stdout, stderr) {
       ts = spawn('tshark', ['-i', 'mon0', '-I', '-f', 'broadcast', '-Y', 'wlan.fc.type == 0 && wlan.fc.subtype == 4 && wlan.sa == d0:e1:40:73:89:7c', '-T', 'fields', '-e', 'frame.time_epoch', '-e', 'wlan.sa', '-e', 'radiotap.dbm_antsignal']);
 
     ts.stdout.on('data', function(data) {
-      console.log('stdout: ' + data);
+      console.log(data);
     });
 
     ts.stderr.on('data', function(data) {
@@ -19,7 +20,29 @@ exec('sudo airmon-ng start wlan1', function(error, stdout, stderr) {
     ts.on('exit', function(code) {
       console.log('child process exited with code ' + code);
     });
-  } else {
-    // handle error
+
+
+
+    var http = require('http');
+
+    var options = {
+      host: 'https://young-beach-90165.herokuapp.com',
+      path: '/signal',
+      method: 'POST'
+    };
+
+    callback = function(response) {
+
+    }
+
+    var req = http.request(options, callback);
+    req.write({"mac": "d0:e1:40:73:89:7c"});
+    req.end();
+
+
+
+
+
+
   }
 });
