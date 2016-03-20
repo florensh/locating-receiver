4b1e078ccc359445c9d660c6febcecb28622aa34
florensh <florensh@gmx.de>
2016-03-20 13:46:15 +0100

    test

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ console.log('starting tshark');
 var sys = require('sys'),
   exec = require('child_process').exec;
 exec('sudo airmon-ng start wlan1', function(error, stdout, stderr) {
-
   if (!error) {
     // print the output
     sys.puts(stdout);
     var spawn = require('child_process').spawn,
-      ts = spawn('tshark', ['-i', 'mon0', '-I', '-f', 'broadcast', '-Y', 'wlan.fc.type == 0 && wlan.fc.subtype == 4', '-T', 'fields', '-e', 'frame.time_epoch', '-e', 'wlan.sa', '-e', 'radiotap.dbm_antsignal']);
-
+    // am = spawn('airmon-ng', ['start', 'wlan1']),
+    ts = spawn('tshark', ['-i', 'mon0', '-I', '-f', 'broadcast', '-Y', 'wlan.fc.type == 0 && wlan.fc.subtype == 4', '-T', 'fields', '-e', 'frame.time_epoch', '-e', 'wlan.sa', '-e', 'radiotap.dbm_antsignal']);
+    // ts = spawn('tshark', ['-i', 'mon0']);
     ts.stdout.on('data', function(data) {
-      console.log(data);
+      console.log('stdout: ' + data);
     });
 
     ts.stderr.on('data', function(data) {
@@ -20,5 +20,25 @@ exec('sudo airmon-ng start wlan1', function(error, stdout, stderr) {
     ts.on('exit', function(code) {
       console.log('child process exited with code ' + code);
     });
+  } else {
+    // handle error
   }
 });
+
+
+
+
+
+
+
+// am.stdout.on('data', function(data) {
+//   console.log('stdout: ' + data);
+// });
+//
+// am.stderr.on('data', function(data) {
+//   console.log('stderr: ' + data);
+// });
+//
+// am.on('exit', function(code) {
+//   console.log('child process exited with code ' + code);
+// });
