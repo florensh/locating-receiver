3bfacb6102f1563b0ee4e8bb45ea6f4d1d36ac01
florensh <florensh@gmx.de>
2016-03-21 00:06:22 +0100

    test

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 console.log('starting receiver');
 
-var sys = require('sys'),
-  exec = require('child_process').exec;
-
-
+var exec = require('child_process').exec;
 
 var runCapturing = function(callback) {
-  sys.puts(stdout);
   var spawn = require('child_process').spawn,
     ts = spawn('tshark', ['-i', 'mon0', '-f', 'broadcast', '-Y', 'wlan.fc.type == 0 && wlan.fc.subtype == 4 && wlan.sa == d0:e1:40:73:89:7c', '-T', 'fields', '-e', 'frame.time_epoch', '-e', 'wlan.sa', '-e', 'radiotap.dbm_antsignal']);
   ts.stdout.on('data', function(data) {
@@ -38,14 +34,14 @@ var request = require('request');
 
 // Configure the request
 var options = {
-    url: 'https://young-beach-90165.herokuapp.com/people',
-    method: 'GET'
+  url: 'https://young-beach-90165.herokuapp.com/people',
+  method: 'GET'
 }
 
 // Start the request
-request(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        // Print out the response body
-        console.log(body)
-    }
+request(options, function(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    // Print out the response body
+    console.log(body)
+  }
 })
