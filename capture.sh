990e0cdf4ae661d46a1c258d99e445bf79b5092f
Florens Hückstädt <florensh@gmx.de>
2017-05-07 20:30:02 +0200

    added sleep mode

--- capture.sh
+++ capture.sh
@@ -533,23 +533,26 @@ _capture(){
   | while read epoch sa antsignal sa_resolved ssid; do
     rssi=$(echo $antsignal | cut -f1 -d,)
     current_hour=$(date +"%H")
-    if((_BACKEND && (current_hour<20 || current_hour>21)))
+    if((_BACKEND))
     then
-      # sending the signals to the backend
-      curl "$_BACKEND_URL$_POST_URI" \
-        --silent \
-        -i \
-        -H "Content-Type:application/json" \
-        -d \
-        "
-        {
-          \"timestamp\" : \"$(date -d @$epoch -u +"%Y-%m-%dT%H:%M:%SZ")\",
-          \"mac\" : \"$sa\",
-          \"ssid\" : \"$ssid\",
-          \"receiverUuid\" : \"$_RECEIVER_UUID\",
-          \"rssi\" : \"$rssi\"
-        }
-        " > /dev/null ;
+      if [ $current_hour<1 || $current_hour>8]; then
+
+        # sending the signals to the backend
+        curl "$_BACKEND_URL$_POST_URI" \
+          --silent \
+          -i \
+          -H "Content-Type:application/json" \
+          -d \
+          "
+          {
+            \"timestamp\" : \"$(date -d @$epoch -u +"%Y-%m-%dT%H:%M:%SZ")\",
+            \"mac\" : \"$sa\",
+            \"ssid\" : \"$ssid\",
+            \"receiverUuid\" : \"$_RECEIVER_UUID\",
+            \"rssi\" : \"$rssi\"
+          }
+          " > /dev/null ;
+      fi
     fi
       # print the captured signals to the console
       printf "mac: %s, rssi: %s, ssid: %s\n" $sa $rssi $ssid
